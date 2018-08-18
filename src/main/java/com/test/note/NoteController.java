df3f5437ea69bd833fa699b4cfd1fc4ada93090c
EvgeniyKhoroshev <sonik1995@gmail.com>
2018-08-18 14:43:56 +0300

    spring repository added/ save note form added

--- src/main/java/com/test/note/NoteController.java
+++ src/main/java/com/test/note/NoteController.java
@@ -5,6 +5,7 @@ import com.test.note.repos.NoteRepos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
@@ -25,6 +26,18 @@ public class NoteController {
     {
         Iterable<Note> notes = nRepos.findAll();
         model.put("notes", notes);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String tag, @RequestParam String text, Map<String, Object> model)
+    {
+        Note note = new Note(text, tag);
+        nRepos.save(note);
+
+        Iterable<Note> notes = nRepos.findAll();
+        model.put("notes", notes);
+
         return "main";
     }
 }
