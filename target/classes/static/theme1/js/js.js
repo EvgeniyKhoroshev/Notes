08ac8cd07c84eccfdb95a0cec1ef59c968866a00
EvgeniyKhoroshev <sonik1995@gmail.com>
2018-08-22 00:14:57 +0300

    ready/full

--- target/classes/static/theme1/js/js.js
+++ target/classes/static/theme1/js/js.js
@@ -1,17 +1,3 @@
-	var input = document.body.children[0];
-
-
 	(function () {
-
-    function httpGet(theUrl)
-    {
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-        xmlHttp.send( null );
-        return xmlHttp.responseText;
-    }
-
-      input.oninput.getElementById("filter_text") = function() {
-        var el = document.getElementById('filter_text');
-      };
+      
 	}());
